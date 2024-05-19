26eb7c9fa6e145f754ef3bd803fa3c4a3f7730e8
Olta Olloni <oltaxx@gmail.com>
2024-05-19 16:40:09 +0200

    Added debuggers to see the keys generated

--- src/Test/DHServer.java
+++ src/Test/DHServer.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.*;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.Scanner;
 import javax.crypto.KeyAgreement;
 import javax.crypto.SecretKey;
@@ -30,6 +31,8 @@ public class DHServer {
         privateKey = keyPair.getPrivate();
         publicKey = keyPair.getPublic();
 
+        System.out.println("\033[32mServer DH public key: " + Base64.getEncoder().encodeToString(publicKey.getEncoded()) + "\033[0m");
+
         // Initialize the KeyAgreement
         keyAgree = KeyAgreement.getInstance(ALGORITHM);
         keyAgree.init(privateKey);
@@ -39,6 +42,8 @@ public class DHServer {
         KeyPair signatureKeyPair = generateRSAKeyPair();
         PrivateKey signaturePrivateKey = signatureKeyPair.getPrivate();
         PublicKey signaturePublicKey = signatureKeyPair.getPublic();
+
+        System.out.println("\033[32mServer RSA public key: " + Base64.getEncoder().encodeToString(signaturePublicKey.getEncoded()) + "\033[0m");
 
         ServerSocket serverSocket = new ServerSocket(PORT);
         System.out.println("Listening for connections...");
@@ -76,12 +81,16 @@ public class DHServer {
                 X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(clientPubKeyEnc);
                 PublicKey clientPubKey = keyFactory.generatePublic(x509KeySpec);
 
+                System.out.println("\033[32mClient DH public key: " + Base64.getEncoder().encodeToString(clientPubKeyEnc) + "\033[0m");
+
                 // Perform phase 1 of key agreement
                 keyAgree.doPhase(clientPubKey, true);
 
                 // Generate shared secret
                 byte[] sharedSecret = keyAgree.generateSecret();
                 sharedSecretKey = new SecretKeySpec(sharedSecret, 0, 16, "AES");
+
+                System.out.println("\033[32mShared secret (AES key): " + Base64.getEncoder().encodeToString(sharedSecretKey.getEncoded()) + "\033[0m");
 
                 // Send signed welcome message and public key
                 String welcomeMessage = "Welcome to the secure server!";
